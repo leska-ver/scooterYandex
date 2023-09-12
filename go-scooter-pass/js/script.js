9513e8a7dd3900fbb5d4f967beba9d6d9754b2af
leska-ver <lyaisan.333@yandex.ru>
2023-09-12 13:29:57 +0500

    scooterYandex

---
+++ go-scooter-pass/js/script.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  //Слайдер Каталог
+  const swiperOb = document.querySelector(".evenemang")// Для обёртки if
+  if (swiperOb) { // Обёртка if. Спасение Gulp-а от null в браузере
+    
+  //Эксклюзивный доступ к событиям Самокатов slider
+  let evenemangSlider = new Swiper(".slides-container", {
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row"
+    },
+    spaceBetween: 20,
+
+    /*//Бесконечное листание страниц
+    speed: 2000, //Интервал ожидания
+
+    autoplay: {
+      delay: 3000,//Интервал ожидания
+        disableOnInteraction: false,      
+    }, */
+
+    pagination: {
+      el: ".slider__section .slider-pagination",
+      type: "fraction"
+    },
+    navigation: {
+      nextEl: ".slider-btn_next",
+      prevEl: ".slider-btn_prev"
+    },
+  
+    breakpoints: {
+      555: {
+        slidesPerView: 1,
+        grid: {
+          rows: 1
+        },
+        spaceBetween: 20
+      },
+      800: {
+        slidesPerView: 2,
+        grid: {
+          rows: 1
+        },
+        spaceBetween: 20
+      },
+      
+      1279: {
+        slidesPerView: 2,
+        grid: {
+          rows: 1
+        },
+        spaceBetween: 20
+      },
+      
+      1439: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2
+        },
+        spaceBetween: 20
+      },
+      
+      2400: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2
+        },
+        spaceBetween: 30
+      }
+    },
+  
+    a11y: false
+  
+    // on: {
+    //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+    //   beforeResize: function () {
+    //     this.slides.forEach((el) => {
+    //       el.style.marginTop = "";
+    //     });
+    //   }
+    // }
+  });
+
+
+  }; //Обёртка if
+
+
+});
